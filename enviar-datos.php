eca99ce9a3bddfc064f205f2115a3bd908cc6431
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-10-20 16:59:08 +0200

    Añadir recogida de datos del checkbox

--- enviar-datos.php
+++ enviar-datos.php
@@ -7,6 +7,7 @@
         "idTema" => $_POST["tema"],
         "idGrupo" => $_POST["grupo"],
         "texto" => $_POST["mensaje"],
+        "contacto" => $_POST["contacto"]
     ];
 
     $sql = "INSERT INTO sugerencias (Texto, idTema, idGrupo) VALUES (
@@ -20,6 +21,31 @@
         echo "Insert creado correctamente";
     }else{
         echo "ERROR" . $conexion->error;
+    }
+
+    /* Ya que los ID son AUTO_INCREMENT debo obtener el ultimo ID que has
+    sido añadido para coger dicho ID y junto con cada elemento de
+    array contacto[] guardarlo en mi tabla del multivaluado */
+
+    $idSugerencia = $conexion -> insert_id;
+
+    if(!empty($_datos['contacto'])){
+        foreach($_datos['contacto'] as $contacto){
+            $sqlContacto = "INSERT INTO Contacto (idSugerencia, Contacto) VALUES (
+                    {$idSugerencia}, '{$contacto}'
+                )";
+            echo $sqlContacto;
+
+            $resultadoContacto = $conexion->query($sqlContacto);
+
+            if($resultadoContacto){
+                echo "Contacto registrado correctamente";
+            }else{
+                echo "ERROR" . $conexion->error;
+            }
+        }
+    }else{
+        echo "No se ha seleccionado ningun medio de contacto";
     }
 
     $conexion->close();
