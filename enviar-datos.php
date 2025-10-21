047d23353316b452e43c6f687343081807d62419
Aitor-Gocerra <aitorgomezcerrato.guadalupe@alumnado.fundacionloyola.net>
2025-10-21 17:18:29 +0200

    Version checkbox obligatorios

--- enviar-datos.php
+++ enviar-datos.php
@@ -3,53 +3,48 @@
     require "config.php";
     $conexion->select_db("los_mismos");
 
-    $_datos = [
-        "idTema" => $_POST["tema"],
-        "idGrupo" => $_POST["grupo"],
-        "texto" => $_POST["mensaje"],
-        "contacto" => $_POST["contacto"]
-    ];
+    // Verifico que el $_Post me esta mandando datos que yo creo obligatorios
+    if(!isset($_POST["tema"]) || !isset($_POST["grupo"]) || !isset($_POST["mensaje"])){
+        die("Error: Faltan datos por rellenar");
+    }
 
-    $sql = "INSERT INTO sugerencias (Texto, idTema, idGrupo) VALUES (
-        '{$_datos['texto']}', {$_datos['idTema']}, {$_datos['idGrupo']}
-    )";
+    // Verifico que haya seleccionado al menos un contacto
+    if(!isset($_Post["contacto"]) || empty($_POST["contacto"])){
+        header("Location: inicio.php");
+    }
+
+    $idTema = $_POST["tema"];
+    $idGrupo = $_POST["grupo"];
+    $texto = $_POST["mensaje"];
+    $contactos = $_POST["contacto"];
+
+    $sql = "INSERT INTO sugerencias (Texto, idTema, idGrupo) VALUES ('{$texto}', {$idTema}, {$idGrupo})";
     echo $sql;
 
     $resultado = $conexion->query($sql);
-
-    if($resultado > 0){
-        echo "Insert creado correctamente";
-    }else{
-        echo "ERROR" . $conexion->error;
-    }
 
     /* Ya que los ID son AUTO_INCREMENT debo obtener el ultimo ID que has
     sido añadido para coger dicho ID y junto con cada elemento de
     array contacto[] guardarlo en mi tabla del multivaluado */
 
     $ultimoId = $conexion -> insert_id;
-
-    if(!empty($_datos['contacto'])){
         
-        foreach($_datos['contacto'] as $idContacto){
-            $sqlContacto = "INSERT INTO SUGERENCIA_CONTACTO (idSugerencia, idContacto) VALUES (
-                {$ultimoId}, {$idContacto}
-            )";
-            
-            echo $sqlContacto;
+    foreach($_datos['contacto'] as $idContacto){
+        $sqlContacto = "INSERT INTO SUGERENCIA_CONTACTO (idSugerencia, idContacto) VALUES (
+            {$ultimoId}, {$idContacto}
+        )";
+        
+        echo $sqlContacto;
 
-            $resultadoContacto = $conexion->query($sqlContacto);
+        $resultadoContacto = $conexion->query($sqlContacto);
 
-            if($resultadoContacto){
-                echo "Contacto registrado correctamente";
-            }else{
-                echo "ERROR" . $conexion->error;
-            }
+        if($resultadoContacto){
+            echo "Contacto registrado correctamente";
+        }else{
+            echo "ERROR" . $conexion->error;
         }
-    }else{
-        echo "No se ha seleccionado ningun medio de contacto";
     }
-
+   
     $conexion->close();
 
 ?>
